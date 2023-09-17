b77afcbb0386ef9e5ae053d2135605ecb61c7cb5
LiHongFei <2529574569@qq.com>
2023-09-17 09:40:31 +0800

    尚医通项目第一天

---
+++ service/service_user/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @Author LiHongFei
+ * @Date 2023 09 16 23 17
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
